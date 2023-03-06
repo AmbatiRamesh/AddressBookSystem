5b25dc28032680fd0f73c42a4a63e2fb6f52ddbd
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-03-06 12:04:52 +0530

    Added: Ability to View Personin by City or State

--- AddressBook.java
+++ AddressBook.java
@@ -11,6 +11,7 @@ public class AddressBook {
                 System.out.println("1. Add New Contact\n2. Edit Contact\n3. Delete Contact" + "\n4. Display Contact\n5. Search by State\n6. Exit");
                 System.out.println("Enter Choice: ");
                 choice =scanner.nextInt();
+
                 switch (choice) {
                     case 1:
                         addressBookMain.addNewContact();
@@ -22,25 +23,50 @@ public class AddressBook {
                         addressBookMain.deleteContact();
                         break;
                     case 4:
-                        addressBookMain.displayContact();
-                        break;
+                        do {
+                            System.out.println("---------------------------");
+                            System.out.println("1. Display Contacts\n2.Display by City\n3. Display by State\n4. Exit");
+                            System.out.println("Choice :");
+                            choice=scanner.nextInt();
+                            switch (choice) {
+                                case 1:
+                                    addressBookMain.displayContact();
+                                    break;
+                                case 2:
+                                    addressBookMain.countByCity();
+                                    break;
+                                case 3:
+                                    addressBookMain.countByState();
+                                    break;
+                                case 4:
+                                    System.out.println("Thank you");
+                                    break;
+                                default:
+                                    System.out.println("Please Select Valid Input");
+                                    break;
+                            }
+                        }while (choice!=4);
                     case 5:
-                        System.out.println("1. Search by City\n2. Search by State\n3. Exit");
-                        System.out.println("Choice :");
-                        choice=scanner.nextInt();
-                        switch (choice){
-                            case 1:
-                                addressBookMain.searchByCity();
-                                break;
-                            case 2:
-                                addressBookMain.searchByState();
-                                break;
-                            case 3:
-                                System.out.println("Thank you");
-                            default:
-                                System.out.println("Please Select Valid Input");
-                        }
-                        break;
+                        do {
+                            System.out.println("-----------------------------");
+                            System.out.println("1. Search by City\n2. Search by State\n3. Exit");
+                            System.out.println("Choice :");
+                            choice = scanner.nextInt();
+                            switch (choice) {
+                                case 1:
+                                    addressBookMain.searchByCity();
+                                    break;
+                                case 2:
+                                    addressBookMain.searchByState();
+                                    break;
+                                case 3:
+                                    System.out.println("Thank you");
+                                    break;
+                                default:
+                                    System.out.println("Please Select Valid Input");
+                                    break;
+                            }
+                        }while (choice!=3);
                     case 6:
                         System.out.println("Thank You for Using Address Book.");
                         break;
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -117,4 +117,15 @@ public class AddressBookMain {
         String state = scanner.next();
         list.stream().filter(contacts -> contacts.getState().equalsIgnoreCase(state)).forEach(contacts -> System.out.println(contacts));
     }
+    public void countByCity() {
+        System.out.println("Enter the city Name:");
+        String city = scanner.next();
+        list.stream().filter(contacts -> contacts.getCity().equalsIgnoreCase(city)).forEach(contacts -> System.out.println(contacts));
+    }
+
+    public void countByState() {
+        System.out.println("Enter the State Name:");
+        String state = scanner.next();
+        list.stream().filter(contacts -> contacts.getState().equalsIgnoreCase(state)).forEach(contacts -> System.out.println(contacts));
+    }
 }
